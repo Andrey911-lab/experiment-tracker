874f142112ed0ec2d5924c0ed473b3270f737a19
Andrey <andrei.harl4mov@yandex.ru>
2026-04-08 18:43:58 +0300

    Добавил функцию для добавления экспериментов испр

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 
 function App() {
+    const [experiments, setExperiments] = useState([]);
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('План');
+
+    const addExperiment = () => {
+        if (name === '') return;
+        const newExp = {
+            id: Date.now(),
+            name: name,
+            status: status
+        };
+        setExperiments([...experiments, newExp]);
+        setName('');
+        setStatus('План');
+    };
+
     return (
         <div>
             <h1>Учёт экспериментов</h1>
